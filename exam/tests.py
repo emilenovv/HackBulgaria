0549091c91cae5779c6a52145526663e54ce989e
Emil Velikov <emvelikov@gmail.com>
2014-12-08 15:45:42 +0200

    Added tests and some changes to tic_tac_toe

--- exam/tests.py
+++ exam/tests.py
@@ -1,31 +1,73 @@
-import tic_tac_toe
+from tic_tac_toe import TicTacToe
 import unittest
 
 
 class TicTacToeTests(unittest.TestCase):
     def setUp(self):
-        self.board = [" "] * 9
+        self.tic = TicTacToe()
 
     def test_is_winner(self):
-        self.board[6] = 'X'
-        self.board[4] = 'X'
-        self.board[2] = 'X'
-        self.assertTrue(tic_tac_toe.is_winner(self.board, 'X'))
+        self.tic.board[6] = 'X'
+        self.tic.board[7] = 'X'
+        self.tic.board[8] = 'X'
+        self.assertTrue(self.tic.is_winner('X'))
 
-    def test_win_successfully(self):
-        self.board[0] = 'X'
-        self.board[1] = ' '
-        self.board[2] = 'O'
-        self.board[3] = ' '
-        self.board[4] = 'X'
-        self.board[5] = 'X'
-        self.board[6] = 'O'
-        self.board[7] = ' '
-        self.board[8] = 'O'
-        print()
-        tic_tac_toe.draw_board(self.board)
-        self.assertEqual(tic_tac_toe.computer_choose_position(self.board, 'O', 4), 7)
+    def test_is_winning(self):
+        self.tic.board[3] = 'O'
+        self.tic.board[4] = 'X'
+        self.tic.board[5] = 'X'
+        self.assertFalse(self.tic.is_winner('O'))
 
+    def test_try_to_win_successfully(self):
+        self.tic.computer_symbol = "O"
+        self.tic.player_symbol = "X"
+        self.tic.board[0] = 'X'
+        self.tic.board[1] = ' '
+        self.tic.board[2] = 'O'
+        self.tic.board[3] = ' '
+        self.tic.board[4] = 'X'
+        self.tic.board[5] = 'X'
+        self.tic.board[6] = 'O'
+        self.tic.board[7] = ' '
+        self.tic.board[8] = 'O'
+        self.tic.free_positions = [1, 3, 7]
+        self.assertEqual(self.tic.computer_choose_position(3), 7)
+
+    def test_config(self):
+        self.tic.computer_symbol = "O"
+        self.tic.player_symbol = "X"
+        self.tic.board[0] = 'X'
+        self.tic.board[1] = ' '
+        self.tic.board[2] = 'O'
+        self.tic.board[3] = 'O'
+        self.tic.board[4] = 'X'
+        self.tic.board[5] = 'X'
+        self.tic.board[6] = ' '
+        self.tic.board[7] = ' '
+        self.tic.board[8] = 'O'
+        self.tic.free_positions = [1, 6, 7]
+        self.tic.make_move(7, 'X')
+        self.assertEqual(self.tic.computer_choose_position(3), 6)
+
+    def test_computer_block_player(self):
+        self.tic.computer_symbol = "O"
+        self.tic.player_symbol = "X"
+        self.tic.board[0] = 'X'
+        self.tic.board[1] = ' '
+        self.tic.board[2] = ' '
+        self.tic.board[3] = 'O'
+        self.tic.board[4] = 'X'
+        self.tic.free_positions = [1, 5, 6, 7, 8]
+        self.assertEqual(self.tic.computer_choose_position(1), 8)
+
+    def test_computer_chooses_corner(self):
+        self.tic.computer_symbol = "O"
+        self.tic.player_symbol = "X"
+        self.tic.board[4] = 'X'
+        move = self.tic.computer_choose_position(1)
+        self.tic.free_positions = [0, 1, 2, 3, 5, 6, 7, 8]
+        corners = [0, 2, 6, 8]
+        self.assertIn(move, corners)
 
 
 
--- exam/tic_tac_toe.py
+++ exam/tic_tac_toe.py
@@ -1,189 +1,181 @@
-from random import choice, randrange
+from random import choice
 
 
-def draw_board(board):
-    line = " | "
-    horizontal_lines = '-' * 11
-    print(" " + board[6] + line + board[7] + line + board[8])
-    print(horizontal_lines)
-    print(" " + board[3] + line + board[4] + line + board[5])
-    print(horizontal_lines)
-    print(" " + board[0] + line + board[1] + line + board[2])
+class TicTacToe:
+    def __init__(self):
+        self.board = [" "] * 9
+        self.free_positions = [x for x in range(9)]
 
+    def draw_board(self):
+        line = " | "
+        horizontal_lines = '-' * 11
+        print(" " + self.board[6] + line + self.board[7] + line + self.board[8])
+        print(horizontal_lines)
+        print(" " + self.board[3] + line + self.board[4] + line + self.board[5])
+        print(horizontal_lines)
+        print(" " + self.board[0] + line + self.board[1] + line + self.board[2])
 
-def choose_between_X_or_O():
-    while True:
-        symbol = input("With what do you want to play - X or O?> ").upper()
-        if symbol == "X":
-            return "X", "O"
-        else:
-            return "O", "X"
-        print("Please, choose between X and O!")
+    def choose_between_X_or_O(self):
+        while True:
+            self.player_symbol = input("With what do you want to play - X or O?> ").upper()
+            if self.player_symbol == "X":
+                self.computer_symbol = "O"
+                break
+            elif self.player_symbol == "O":
+                self.computer_symbol = "X"
+                break
+            print("Please, choose between X and O!")
 
+    def player_choose_position(self):
+        print("Every field corresponds to the number on your numpad")
+        while True:
+            place = int(input('''Select where to put {}. > '''.format(self.player_symbol)))
+            if place >= 1 and place <= 9:
+                if self.is_place_free(place - 1) is True:
+                    actual_place = place - 1
+                    self.free_positions.remove(actual_place)
+                    return actual_place
+                else:
+                    print("This place is taken. Choose another.")
+            print("Select between 1 and 9!")
 
-def player_choose_position(board, symbol):
-    print("Every field corresponds to the number on your numpad")
-    while True:
-        place = int(input('''Select where to put {}. > '''.format(symbol)))
-        if place >= 1 and place <= 9:
-            if is_place_free(board, place - 1) is True:
-                actual_place = place - 1
-                return actual_place
-            else:
-                print("This place is taken. Choose another.")
-        print("Select between 1 and 9!")
+    def is_place_free(self, position):
+        return position in self.free_positions
 
+    def is_board_full(self):
+        return len(self.free_positions) == 0
 
-def is_place_free(board, position):
-    return board[position] == " "
+    def choose_move_from_list(self, list):
+        possible_moves = []
+        for move in list:
+            if self.is_place_free(move):
+                possible_moves.append(move)
 
+        if len(possible_moves) != 0:
+            return choice(possible_moves)
+        return False
 
-def is_board_full(board):
-    for place in board:
-        if place == " ":
-            return False
-    return True
+    def computer_try_to_win(self):
+        for i in range(9):
+            if self.is_place_free(i):
+                self.board[i] = self.computer_symbol
+                if self.is_winner(self.computer_symbol):
+                    self.board[i] = " "
+                    self.free_positions.remove(i)
+                    return i
+                self.board[i] = " "
+        return False
 
+    def computer_try_to_block(self):
+        for i in range(9):
+            if self.is_place_free(i):
+                self.board[i] = self.player_symbol
+                if self.is_winner(self.player_symbol):
+                    self.board[i] = " "
+                    self.free_positions.remove(i)
+                    return i
+                self.board[i] = " "
+        return False
 
-def choose_move_from_list(board, list):
-    possible_moves = []
-    for move in list:
-        if is_place_free(board, move):
-            possible_moves.append(move)
-
-    if len(possible_moves) != 0:
-        return choice(possible_moves)
-    return False
-
-
-def computer_choose_position(board, player_symbol, num_computer_symbols):
-    if player_symbol == "X":
-        computer_symbol = "O"
-    else:
-        computer_symbol = "X"
-
-    #proverqva dali moje da bie v slevashtiq hod
-    for i in range(9):
-        if is_place_free(board, i):
-            board[i] = computer_symbol
-            if is_winner(board, computer_symbol):
-                print(i)
-                board[i] = " "
-                return i
-            board[i] = " "
-
-    #opitva se da blokira ako moje
-    for i in range(9):
-        if is_place_free(board, i):
-            board[i] = player_symbol
-            if is_winner(board, player_symbol):
-                board[i] = " "
-                return i
-            board[i] = " "
-
-    corners = [0, 2, 6, 8]
-    move = choose_move_from_list(board, corners)
-    if move is not False:
-        return move
-
-    #opitva se da sloji simvol v protivopolojniq ygyl
-    if num_computer_symbols == 1:
-        if board[0] == computer_symbol and is_place_free(board, 8):
-            return 8
-        elif board[8] == computer_symbol and is_place_free(board, 0):
-            return 0
-        elif board[2] == computer_symbol and is_place_free(board, 6):
-            return 6
-        elif board[6] == computer_symbol and is_place_free(board, 2):
-            return 2
-
-    if num_computer_symbols == 2:
-        if board[0] == computer_symbol and board[8] == computer_symbol:
-            if is_place_free(board, 6):
-                return 6
-            elif is_place_free(board, 2):
-                return 2
-        if board[6] == computer_symbol and board[2] == computer_symbol:
-            if is_place_free(board, 0):
-                return 0
-            elif is_place_free(board, 2):
-                return 2
-
-        if is_place_free(board, 4):
-            return 4
-
-        move = choose_move_from_list(board, corners)
+    def computer_choose_position(self, num_computer_symbols):
+        corners = [0, 2, 6, 8]
+        move = self.choose_move_from_list(corners)
         if move is not False:
+            self.free_positions.remove(move)
             return move
 
-        sides = [1, 3, 6, 8]
-        move = choose_move_from_list(board, sides)
-        return move
+        #opitva se da sloji simvol v protivopolojniq ygyl
+        elif num_computer_symbols == 1:
+            if self.board[0] == self.computer_symbol and self.is_place_free(8):
+                self.free_positions.remove(8)
+                return 8
+            elif self.board[8] == self.computer_symbol and self.is_place_free(0):
+                self.free_positions.remove(0)
+                return 0
+            elif self.board[2] == self.computer_symbol and self.is_place_free(6):
+                self.free_positions.remove(6)
+                return 6
+            elif self.board[6] == self.computer_symbol and self.is_place_free(2):
+                self.free_positions.remove(2)
+                return 2
 
+        elif num_computer_symbols == 2:
+            if self.board[0] == self.computer_symbol and self.board[8] == self.computer_symbol:
+                if self.is_place_free(6):
+                    self.free_positions.remove(6)
+                    return 6
+                elif self.is_place_free(2):
+                    self.free_positions.remove(2)
+                    return 2
+            elif self.board[6] == self.computer_symbol and self.board[2] == self.computer_symbol:
+                if self.is_place_free(0):
+                    self.free_positions.remove(0)
+                    return 0
+                elif self.is_place_free(8):
+                    self.free_positions.remove(8)
+                    return 2
 
-def make_move(board, move, symbol):
-    board[move] = symbol
+            elif self.is_place_free(4):
+                self.free_positions.remove(4)
+                return 4
 
+            move = self.choose_move_from_list(corners)
+            if move is not False:
+                self.free_positions.remove(move)
+                return move
 
-def player_starts_first():
-    random_num = randrange(2)
-    if random_num == 0:
-        return False
-    return False
+            sides = [1, 3, 5, 7]
+            move = self.choose_move_from_list(sides)
+            self.free_positions.remove(move)
+            return move
 
+    def make_move(self, move, symbol):
+        self.board[move] = symbol
 
-def is_winner(board, symbol):
-    is_winner_flag = True
-    winning_combinations = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
-                            (0, 3, 6), (7, 4, 1), (8, 5, 2),
-                            (0, 4, 8), (6, 4, 2)]
-    for comb in winning_combinations:
+    def is_winner(self, symbol):
         is_winner_flag = True
-        for position in comb:
-            if board[position] != symbol:
-                is_winner_flag = False
-        if is_winner_flag is True:
-            print(symbol)
-            print(comb)
-            return True
-    return False
+        winning_combinations = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
+                                (0, 3, 6), (7, 4, 1), (8, 5, 2),
+                                (0, 4, 8), (6, 4, 2)]
+        for comb in winning_combinations:
+            is_winner_flag = True
+            for position in comb:
+                if self.board[position] != symbol:
+                    is_winner_flag = False
+            if is_winner_flag is True:
+                return True
+        return False
 
-
-def do_want_to_play_again():
-    while True:
-        answer = input("Do you wanna play again - yes or no? ")
-        if answer == "yes":
-            return True
-        elif answer == "no":
-            return False
-        print("Just answer - yes or no? ")
+    def do_want_to_play_again(self):
+        while True:
+            answer = input("Do you wanna play again - yes or no? ")
+            if answer == "yes":
+                return True
+            elif answer == "no":
+                return False
+            print("Just answer - yes or no? ")
 
 
 def main():
     while True:
-        board = [" "] * 9
+        tictactoe = TicTacToe()
         num_computer_symbols = 0
-        player_symbol, computer_symbol = choose_between_X_or_O()
-        print("Player symbol {}".format(player_symbol))
-        print("Computer symbol {}".format(computer_symbol))
-        player_turn = player_starts_first()
-        if player_turn is True:
-            print("You will go first!")
-        else:
-            print("Computer will go first! Good luck!")
+        tictactoe.choose_between_X_or_O()
+        print("You will go first!")
+        player_turn = True
         while True:
             if player_turn is True:
                 print("Player turn")
-                draw_board(board)
-                move = player_choose_position(board, player_symbol)
-                make_move(board, move, player_symbol)
-                if is_winner(board, player_symbol):
-                    draw_board(board)
+                tictactoe.draw_board()
+                move = tictactoe.player_choose_position()
+                tictactoe.make_move(move, tictactoe.player_symbol)
+                if tictactoe.is_winner(tictactoe.player_symbol):
+                    tictactoe.draw_board()
                     print("You won! Congrats!")
                     break
                 else:
-                    if is_board_full(board) is True:
-                        draw_board(board)
+                    if tictactoe.is_board_full() is True:
+                        tictactoe.draw_board()
                         print("No one wins!")
                         break
                     else:
@@ -192,23 +184,22 @@ def main():
             #red na kompiutyra
             else:
                 print("Computer turn")
-                move = computer_choose_position(board, computer_symbol, num_computer_symbols)
-                print(move)
-                make_move(board, move, computer_symbol)
+                move = tictactoe.computer_choose_position(num_computer_symbols)
+                tictactoe.make_move(move, tictactoe.computer_symbol)
                 num_computer_symbols += 1
-                if is_winner(board, computer_symbol):
-                    draw_board(board)
+                if tictactoe.is_winner(tictactoe.computer_symbol):
+                    tictactoe.draw_board()
                     print("Sorry, you lost! HAHAHAHA")
                     break
                 else:
-                    if is_board_full(board) is True:
-                        draw_board(board)
+                    if tictactoe.is_board_full() is True:
+                        tictactoe.draw_board()
                         print("No one wins!")
                         break
                     else:
                         player_turn = True
 
-        if do_want_to_play_again() is False:
+        if tictactoe.do_want_to_play_again() is False:
             break
 
 
